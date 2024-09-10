e0586038cb3b4e0a7209a5137f530b220ac77704
Franck <136079691+Freddyede@users.noreply.github.com>
2024-09-10 16:45:32 +0200

    Todo tonight

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -3,13 +3,16 @@ import Stripe from 'stripe';
 import 'dotenv/config';
 import { PaymentLinkService } from "./payment-link.service";
 
+/**
+ * TODO à la création d'un trajet(automation):
+ *  - Récupérer tous les (price_id: 'price_1PxUXjEp12gOpkuFRGLVe7qo') des produits stripe
+ *  - Y ajoute au paymentList
+ *  WARNING: ASYNC DANS ASYNC !!!
+ */
 @Injectable()
 export class StripeService {
+  constructor(private paymentService: PaymentLinkService) {}
 
-  constructor(
-    private paymentService: PaymentLinkService,
-  ) {
-  }
   async getPaymentLists(): Promise<Stripe.PaymentLink[]> {
     await this.paymentService.productService.createProduct({name: 'my test 4FEZ44Z', default_price_data: {unit_amount: 100, currency: 'usd'}});
     let products = await this.paymentService.productService.getProducts();
@@ -17,6 +20,7 @@ export class StripeService {
     await this.paymentService.addPaymentLinkProduct({ price: 'price_1PxUXjEp12gOpkuFRGLVe7qo', quantity: 1 });
     return this.paymentService.getPaymentLinkedLists();
   }
+
   async getProducts(): Promise<Stripe.Product[]> {
     return this.paymentService.productService.getProducts();
   }
