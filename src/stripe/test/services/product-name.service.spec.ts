d7abfb70d4fb594df40c96223daa14f8ec6eb6a0
Franck <136079691+Freddyede@users.noreply.github.com>
2024-09-13 12:29:00 +0200

    Created ProductNameService tests

--- src/stripe/test/services/product-name.service.spec.ts
+++ src/stripe/test/services/product-name.service.spec.ts
@@ -41,4 +41,12 @@ describe('ProductNameService', () => {
     let product: IProduct;
     expect(service.stripeProductName).toEqual(typeof product);
   });
+  it('should be return good object for send it to stripe modules', () => {
+    let obj = {owner: 'Franck', located_at: 'Lyon', located_from: 'Paris', price: 200};
+    service.runner(obj);
+    let product = service.stripeProductName;
+    expect(product.name).toEqual(`${obj.owner}-${obj.located_at}_${obj.located_from}`);
+    expect(product.default_price_data.unit_amount).toEqual(obj.price);
+    expect(product.default_price_data.currency).toEqual(/usd/i);
+  });
 });
