b7f5dc45537cacd2ad4e3a8388cfd0da8df5391c
Franck <136079691+Freddyede@users.noreply.github.com>
2024-09-13 11:45:56 +0200

    Created ProductNameService with their implementation

--- src/stripe/interfaces/index.ts
+++ src/stripe/interfaces/index.ts
@@ -1,7 +1,40 @@
+// Class Interfaces
+
+/**
+ * # Warning: Please don't touch it !
+ * ### Interface for ProductNameService
+ * @version 0.0.1
+ * @author Patouillard Franck<patouillardfranck3@gmail.com>
+ */
+export interface IProductNameService {
+  initProduct(product: IProductDatabase): void;
+  get stripeProductName(): IProduct;
+  runner(): void;
+
+}
+
+// Object Interfaces
+
+/**
+ * Interface for database product
+ * @version 0.0.1
+*/
+export interface IProductDatabase {
+  owner: string;
+  located_at: string;
+  located_from: string;
+  price: number;
+}
+
+/**
+ * Interface for stripe charge
+ * @version 0.0.1
+ */
 export interface ICharges {
   amount: number;
   currency: string;
 }
+
 /**
  * Interface for stripe refund
  */
@@ -19,7 +52,9 @@ export interface IProduct {
     unit_amount: any;
     currency: string;
   }
-}/**
+}
+
+/**
  * Interface for stripe product
  */
 export interface IProductUpdate {
--- src/stripe/services/product-name.service.ts
+++ src/stripe/services/product-name.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { IProduct, IProductDatabase, IProductNameService } from '../interfaces';
 
 /**
- * @author
+ * This service does create new product name with object configuration
+ * @author Dayem Mohammad<mohamad_dayem@yahoo.com>
+ * @version 0.0.1
  */
 @Injectable()
-export class ProductNameService {
-  constructor() { }
+export class ProductNameService implements IProductNameService {
+
+  initProduct(product: IProductDatabase): void {
+    throw new Error('Method not implemented.');
+  }
+  get stripeProductName(): IProduct {
+    throw new Error('Method not implemented.');
+  }
+  runner(): void {
+    throw new Error('Method not implemented.');
+  }
 }
