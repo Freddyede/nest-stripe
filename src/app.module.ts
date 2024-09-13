bde952984a5ee592671c3a3410f370e73cef7059
Franck <136079691+Freddyede@users.noreply.github.com>
2024-09-13 11:25:03 +0200

    Update stripe structure: - CRUD product(done) - CRUD paymentLink(in progress) - CRUD refund(to do)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,6 @@ import { Module } from '@nestjs/common';
 import { StripeModule } from './stripe/modules/stripe.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseConfig } from './config/database.config';
-import { UserEntity } from './stripe/entity/user.entity';
-import { JourneysEntity } from './stripe/entity/journeys.entity';
-import { CityEntity } from './stripe/entity/city.entity';
 
 @Module({
   imports: [
@@ -13,11 +10,7 @@ import { CityEntity } from './stripe/entity/city.entity';
       synchronize: true,
       autoLoadEntities: true,
     }),
-    TypeOrmModule.forFeature([UserEntity]),
-    TypeOrmModule.forFeature([JourneysEntity]),
-    TypeOrmModule.forFeature([CityEntity]),
     StripeModule,
   ],
-  exports: [TypeOrmModule.forFeature([JourneysEntity]), StripeModule],
 })
 export class AppModule {}
