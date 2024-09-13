63828ef6214a1b50926df86f240918e4c7cc1165
Franck <136079691+Freddyede@users.noreply.github.com>
2024-09-13 11:25:58 +0200

    Update stripe structure: - Delete all connection to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,7 @@
 import { Module } from '@nestjs/common';
 import { StripeModule } from './stripe/modules/stripe.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { DatabaseConfig } from './config/database.config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      ...DatabaseConfig,
-      synchronize: true,
-      autoLoadEntities: true,
-    }),
-    StripeModule,
-  ],
+  imports: [StripeModule]
 })
 export class AppModule {}
